35055f503955690bfe7fac3c299a46e61097c9ec
gr33ngr33n <actfun07@gmail.com>
2020-06-18 13:39:19 +0800

    setup firebase config & initialize it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase/app';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'firebase-proj1';
+export class AppComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {
+    firebase.initializeApp(environment.config);
+  }
 }
