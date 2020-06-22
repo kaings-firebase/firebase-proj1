83561ee67f8bc56e24795b7da32e2caebf3e23ac
gr33ngr33n <actfun07@gmail.com>
2020-06-22 09:37:27 +0800

    added setAdminRole function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,26 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+export const setAdminRole = functions.https.onCall((data, context) => {
+
+  console.log('setAdminRole_data..... ', data);
+  console.log('setAdminRole_context..... ', context);
+
+  return admin.auth().getUserByEmail(data.email)
+          .then((user) => {
+            console.log('setAdminRole_user..... ', user);
+
+            return admin.auth().setCustomUserClaims(user.uid, { admin: true });
+          })
+          .then((res) => {
+            console.log('setAdminRole_res..... ', res);
+
+            return { message: `OK! User ${data.email} is now an Admin!` };
+          })
+          .catch((err) => {
+            console.error('setAdminRole_err..... ', err)
+          });
+
+});
